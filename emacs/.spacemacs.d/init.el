7c46b05ad933a6f2777a6ea314db85ae9643bb06
c02y <cody.chan.cz@gmail.com>
2019-05-06 16:25:19 +0800

    add emacs/.spacemacsd to dotfiles

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -485,4 +485,5 @@ This function is called at the very end of Spacemacs initialization."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ )
 )
