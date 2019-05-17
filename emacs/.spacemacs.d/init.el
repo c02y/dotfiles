065675eac9ad13cd4019935076f5791014712c77
c02y <cody.chan.cz@gmail.com>
2019-05-17 15:54:27 +0800

    emacs: using hybrid instead of vim as the editing style

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -177,7 +177,8 @@ It should only modify the values of Spacemacs settings."
    ;; with `:variables' keyword (similar to layers). Check the editing styles
    ;; section of the documentation for details on available variables.
    ;; (default 'vim)
-   dotspacemacs-editing-style 'vim
+   dotspacemacs-editing-style '(hybrid :variables
+                                       hybrid-style-visual-feedback t)
 
    ;; If non-nil output loading progress in `*Messages*' buffer. (default nil)
    dotspacemacs-verbose-loading nil
