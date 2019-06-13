06af7801a6b25498f43f3dce231b593c62e066ce
c02y <cody.chan.cz@gmail.com>
2019-06-13 16:30:16 +0800

    emacs: disable loading recents and projects on startup, speed up of startup of spacemacs

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -222,8 +222,8 @@ It should only modify the values of Spacemacs settings."
    ;; `recents' `bookmarks' `projects' `agenda' `todos'.
    ;; List sizes may be nil, in which case
    ;; `spacemacs-buffer-startup-lists-length' takes effect.
-   dotspacemacs-startup-lists '((recents . 10)
-                                (projects . 10))
+   dotspacemacs-startup-lists '((recents . 0)
+                                (projects . 0))
 
    ;; True if the home buffer should respond to resize events. (default t)
    dotspacemacs-startup-buffer-responsive t
