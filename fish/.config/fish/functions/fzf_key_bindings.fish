d072061b95e113b14f3b7c0fc85d1bdb4acdeeac
c02y <cody.chan.cz@gmail.com>
2019-07-20 18:37:56 +0800

    fish: update fzf_key_bindings.fish according to original file

--- fish/.config/fish/functions/fzf_key_bindings.fish
+++ fish/.config/fish/functions/fzf_key_bindings.fish
@@ -51,7 +51,7 @@ function fzf_key_bindings
             # history's -z flag was added in fish 2.4.0, so don't use it for versions
             # before 2.4.0.
             if [ "$FISH_MAJOR" -gt 2 -o \( "$FISH_MAJOR" -eq 2 -a "$FISH_MINOR" -ge 4 \) ];
-                history -z | eval (__fzfcmd) --read0 -q '(commandline)' | perl -pe 'chomp if eof' | read -lz result
+                history -z | eval (__fzfcmd) --read0 --print0 -q '(commandline)' | read -lz result
                 and commandline -- $result
             else
                 history | eval (__fzfcmd) -q '(commandline)' | read -l result
@@ -62,49 +62,49 @@ function fzf_key_bindings
     end
 
     function fzf-complete -d 'fzf completion and print selection back to commandline'
-	    # As of 2.6, fish's "complete" function does not understand
-	    # subcommands. Instead, we use the same hack as __fish_complete_subcommand and
-	    # extract the subcommand manually.
-	    set -l cmd (commandline -co) (commandline -ct)
-	    switch $cmd[1]
-		    case env sudo
-			    for i in (seq 2 (count $cmd))
-				    switch $cmd[$i]
-					    case '-*'
-					    case '*=*'
-					    case '*'
-						    set cmd $cmd[$i..-1]
-						    break
-				    end
-			    end
-	    end
-	    set cmd (string join -- ' ' $cmd)
+        # As of 2.6, fish's "complete" function does not understand
+        # subcommands. Instead, we use the same hack as __fish_complete_subcommand and
+        # extract the subcommand manually.
+        set -l cmd (commandline -co) (commandline -ct)
+        switch $cmd[1]
+            case env sudo
+                for i in (seq 2 (count $cmd))
+                    switch $cmd[$i]
+                        case '-*'
+                        case '*=*'
+                        case '*'
+                            set cmd $cmd[$i..-1]
+                            break
+                    end
+                end
+        end
+        set cmd (string join -- ' ' $cmd)
 
-	    set -l complist (complete -C$cmd)
-	    set -l result
-	    string join -- \n $complist | sort | eval (__fzfcmd) -m --select-1 --exit-0 --header '(commandline)' | cut -f1 | while read -l r; set result $result $r; end
+        set -l complist (complete -C$cmd)
+        set -l result
+        string join -- \n $complist | sort | eval (__fzfcmd) -m --select-1 --exit-0 --header '(commandline)' | cut -f1 | while read -l r; set result $result $r; end
 
-	    set prefix (string sub -s 1 -l 1 -- (commandline -t))
-	    for i in (seq (count $result))
-		    set -l r $result[$i]
-		    switch $prefix
-			    case "'"
-				    commandline -t -- (string escape -- $r)
-			    case '"'
-				    if string match '*"*' -- $r >/dev/null
-					    commandline -t --  (string escape -- $r)
-				    else
-					    commandline -t -- '"'$r'"'
-				    end
-			    case '~'
-				    commandline -t -- (string sub -s 2 (string escape -n -- $r))
-			    case '*'
-				    commandline -t -- (string escape -n -- $r)
-		    end
-		    [ $i -lt (count $result) ]; and commandline -i ' '
-	    end
+        set prefix (string sub -s 1 -l 1 -- (commandline -t))
+        for i in (seq (count $result))
+            set -l r $result[$i]
+            switch $prefix
+                case "'"
+                    commandline -t -- (string escape -- $r)
+                case '"'
+                    if string match '*"*' -- $r >/dev/null
+                        commandline -t --  (string escape -- $r)
+                    else
+                        commandline -t -- '"'$r'"'
+                    end
+                case '~'
+                    commandline -t -- (string sub -s 2 (string escape -n -- $r))
+                case '*'
+                    commandline -t -- (string escape -n -- $r)
+            end
+            [ $i -lt (count $result) ]; and commandline -i ' '
+        end
 
-	    commandline -f repaint
+        commandline -f repaint
     end
 
     function __fzfcmd
