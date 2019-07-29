e579cd0f02d0b2add74ed7ab1d3d8b1a1ee26d37
c02y <cody.chan.cz@gmail.com>
2019-07-29 09:52:25 +0200

    fish: fix gitpr

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1252,7 +1252,7 @@ abbr gitt 'git tag'
 abbr gitft 'git ls-files --error-unmatch' # Check if file/dir is git-tracked
 abbr gitpu 'git push -v'
 abbr gitpl 'git pull -v'
-abbr gitpr 'git pull -v --rebase -i'
+abbr gitpr 'git pull -v --rebase=interactive'
 function gitrm -d 'clean untracked file/dirs(fileA fileB...), all by default)'
     if set -q $argv             # no given argv
         git clean -f -d
