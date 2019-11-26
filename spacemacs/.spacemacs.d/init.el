b3d2424f45c069902cf2a5bf06f990b7f5a22d7a
c02y <cody.chan.cz@gmail.com>
2019-11-26 14:33:02 +0800

    spacemacs: workaround recentf issue

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -743,8 +743,9 @@ before packages are loaded."
    ;; to cause the *compilation* buffer to automatically scroll to the end of new output
    compilation-scroll-output t
    ;; TODO: delete it until the recentf issue is fixed
-   ;; spacemacs/issues/5186#issuecomment-312867664
-   recentf-save-file (format "/tmp/recentf.%s" (emacs-pid))
+   ;; workaround by changing emacs.service: spacemacs/issues/12873#issuecomment-558252242
+   ;; spacemacs/issues/5186#issuecomment-31286766
+   ;; recentf-save-file (format "/tmp/recentf.%s" (emacs-pid))
    )
 
   ;; NOTE: along with undo-tree-auto-save-history
