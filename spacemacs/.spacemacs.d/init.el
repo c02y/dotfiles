6e6dd0d5014eba91e39304643a6b2efa2d923f93
c02y <cody.chan.cz@gmail.com>
2020-05-03 01:06:15 +0800

    spacemacs: undo excluding yasnippet-snippets package since some snppets are using some commands from the package

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -194,7 +194,7 @@ This function should only modify configuration layer settings."
                                     google-c-style auto-highlight-symbol git-gutter
                                     git-gutter-fringe git-gutter-fringe+ fancy-battery
                                     dactyl-mode lorem-ipsum uuidgen evil-tutor indent-guide
-                                    org-bullets yasnippet-snippets
+                                    org-bullets
                                     )
 
    ;; Defines the behaviour of Spacemacs when installing packages.
