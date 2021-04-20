1100fc195b5069c3c9d21f0f4f710e6ad814ecac
c02y <cody.chan.cz@gmail.com>
2021-04-21 02:06:36 +0800

    ranger: improve indentation preview of source code

--- ranger/.config/ranger/scope.sh
+++ ranger/.config/ranger/scope.sh
@@ -306,7 +306,7 @@ handle_mime() {
             env HIGHLIGHT_OPTIONS="${HIGHLIGHT_OPTIONS}" highlight \
                 --out-format="${highlight_format}" \
                 --force -- "${FILE_PATH}" && exit 5
-            env COLORTERM=8bit bat --color=always --style="plain" \
+            env COLORTERM=8bit bat --color=always --style="rule" \
                 -- "${FILE_PATH}" && exit 5
             pygmentize -f "${pygmentize_format}" -O "style=${PYGMENTIZE_STYLE}"\
                 -- "${FILE_PATH}" && exit 5
