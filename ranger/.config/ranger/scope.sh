2ad473acc64439cd2f97fdce420fe2f66090afbf
c02y <cody.chan.cz@gmail.com>
2021-04-25 11:01:33 +0800

    ranger: enable video preview

--- ranger/.config/ranger/scope.sh
+++ ranger/.config/ranger/scope.sh
@@ -153,11 +153,11 @@ handle_image() {
             ## as above), but might fail for unsupported types.
             exit 7;;
 
-        ## Video
-        # video/*)
-        #     # Thumbnail
-        #     ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
-        #     exit 1;;
+        # Video
+        video/*)
+            # Thumbnail
+            ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
+            exit 1;;
 
         ## PDF
         # application/pdf)
