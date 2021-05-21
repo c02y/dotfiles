72d42f155fe94c98b26e4a64ce46b530b37a234d
c02y <cody.chan.cz@gmail.com>
2021-05-21 21:20:42 +0800

    fish: disable preview-window for fzf in pk function

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -429,7 +429,7 @@ function pk --description 'kill processes containing a pattern or PID'
     if set -q $argv
         # this part of using fzf is from
         # https://github.com/SidOfc/dotfiles/blob/e94b96b908479950186e42a3709511a0afe300e4/.config/fish/functions/kp.fish
-        set -l __kp__pid (ps -ef | sed 1d | eval "fzf $FZF_DEFAULT_OPTS -m --header='[kill:process]'" | awk '{print $2}')
+        set -l __kp__pid (ps -ef | sed 1d | eval "fzf $FZF_DEFAULT_OPTS --preview-window hidden -m --header='[kill:process]'" | awk '{print $2}')
         set -l __kp__kc $argv[1]
 
         if test "x$__kp__pid" != x
