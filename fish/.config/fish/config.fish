d1e591033a9eca2ef9f625112adcc2432352faae
c02y <cody.chan.cz@gmail.com>
2021-06-17 07:33:58 +0800

    fish: disable audio in dic

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2445,7 +2445,7 @@ function catt
     end
 end
 
-alias dic 'trans :zh+en -d -show-dictionary Y -v -sp -theme ~/.local/bin/trans-theme-matrix.trans'
+alias dic 'trans :zh+en -d -show-dictionary Y -v -theme ~/.local/bin/trans-theme-matrix.trans'
 
 # count chars of lines of a file
 # awk '{ print length }' | sort -n | uniq -c
