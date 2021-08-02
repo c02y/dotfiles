634fb97415e3075508712c0595cd2c0b6e9baf1a
c02y <cody.chan.cz@gmail.com>
2021-08-03 07:38:38 +0800

    spacemacs: update init.el according to .spacemacs.template

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -78,7 +78,7 @@ This function should only modify configuration layer settings."
                treemacs-show-cursor t
                )
      ;; NOTE: install shellcheck
-	 ;; lsp for bash "npm i -g bash-language-server"
+     ;; lsp for bash "npm i -g bash-language-server"
      shell-scripts
      ;; NOTE: `pip install importmagic epc flake8 pytest nose autoflake "ptvsd>=4.2"
      ;; if using lsp as backend: pip install python-language-server pyls-isort pyls-mypy'
@@ -178,7 +178,7 @@ This function should only modify configuration layer settings."
       ccls-sem-highlight-method 'font-lock
       ;; spacemacs/issues/10051#issuecomment-605979333
       lsp-enable-indentation nil
-	  ;; https://emacs-lsp.github.io/lsp-mode/tutorials/how-to-turn-off/
+      ;; https://emacs-lsp.github.io/lsp-mode/tutorials/how-to-turn-off/
       lsp-headerline-breadcrumb-enable nil
       )
      ;; M-x dap-cpptools-setup after packages are installed by dap layer
@@ -371,6 +371,9 @@ It should only modify the values of Spacemacs settings."
 
    ;; True if the home buffer should respond to resize events. (default t)
    dotspacemacs-startup-buffer-responsive t
+
+   ;; Show numbers before the startup list lines. (default t)
+   dotspacemacs-show-startup-list-numbers t
 
    ;; The minimum delay in seconds between number key presses. (default 0.4)
    dotspacemacs-startup-buffer-multi-digit-delay 0.4
@@ -571,8 +574,8 @@ It should only modify the values of Spacemacs settings."
    ;; If set to `t', `relative' or `visual' then line numbers are enabled in all
    ;; `prog-mode' and `text-mode' derivatives. If set to `relative', line
    ;; numbers are relative. If set to `visual', line numbers are also relative,
-   ;; but lines are only visual lines are counted. For example, folded lines
-   ;; will not be counted and wrapped lines are counted as multiple lines.
+   ;; but only visual lines are counted. For example, folded lines will not be
+   ;; counted and wrapped lines are counted as multiple lines.
    ;; This variable can also be set to a property list for finer control:
    ;; '(:relative nil
    ;;   :visual nil
@@ -598,7 +601,7 @@ It should only modify the values of Spacemacs settings."
 
    ;; If non-nil smartparens-mode will be enabled in programming modes.
    ;; (default t)
-   ;; dotspacemacs-activate-smartparens-mode t
+   dotspacemacs-activate-smartparens-mode t
 
    ;; If non-nil pressing the closing parenthesis `)' key in insert mode passes
    ;; over any automatically added closing parenthesis, bracket, quote, etc...
@@ -666,14 +669,14 @@ It should only modify the values of Spacemacs settings."
    ;; (default nil)
    dotspacemacs-whitespace-cleanup 'trailing
 
-   ;; If non nil activate `clean-aindent-mode' which tries to correct
-   ;; virtual indentation of simple modes. This can interfer with mode specific
+   ;; If non-nil activate `clean-aindent-mode' which tries to correct
+   ;; virtual indentation of simple modes. This can interfere with mode specific
    ;; indent handling like has been reported for `go-mode'.
    ;; If it does deactivate it here.
    ;; (default t)
    dotspacemacs-use-clean-aindent-mode nil
 
-   ;; Accept SPC as y for prompts if non nil. (default nil)
+   ;; Accept SPC as y for prompts if non-nil. (default nil)
    dotspacemacs-use-SPC-as-y t
 
    ;; If non-nil shift your number row to match the entered keyboard layout
@@ -693,7 +696,7 @@ It should only modify the values of Spacemacs settings."
    dotspacemacs-pretty-docs nil
 
    ;; If nil the home buffer shows the full path of agenda items
-   ;; and todos. If non nil only the file name is shown.
+   ;; and todos. If non-nil only the file name is shown.
    dotspacemacs-home-shorten-agenda-source nil
 
    ;; If non-nil then byte-compile some of Spacemacs files.
@@ -1240,7 +1243,7 @@ Emacs session."
     ;; whitespace-cleanup will also do untabify-it/tabify-it automatically
     ;; according indent-tabs-mode, so the definitions of two functions are not needed
     "bc" 'whitespace-cleanup
-	;; original: SPC b N I
+    ;; original: SPC b N I
     "bC" 'clone-indirect-buffer-other-window-without-purpose
     ;; overwrite the default br
     "br" 'revert-buffer-without-asking
