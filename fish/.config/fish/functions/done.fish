43e1d4e7ab055ea6b72da392f74e6565e6ed991e
c02y <cody.chan.cz@gmail.com>
2021-08-16 05:05:45 +0800

    fish: add disappearing timeout for notify-send in done.fish

--- fish/.config/fish/functions/done.fish
+++ fish/.config/fish/functions/done.fish
@@ -252,7 +252,7 @@ if set -q __done_enabled
                     end
                 end
 
-                notify-send --urgency=$urgency --icon=utilities-terminal --app-name=fish "$title" "$message"
+                notify-send -t 10000 --urgency=$urgency --icon=utilities-terminal --app-name=fish "$title" "$message"
 
                 if test "$__done_notify_sound" -eq 1
                     echo -e "\a" # bell sound
