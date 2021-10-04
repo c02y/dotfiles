4b7412db4ba8693f239c37511fddf6733b9597a4
c02y <cody.chan.cz@gmail.com>
2021-10-04 23:36:24 +0800

    ranger: update zoxide.py and its config according to upstream

--- ranger/.config/ranger/plugins/zoxide.py
+++ ranger/.config/ranger/plugins/zoxide.py
@@ -2,17 +2,16 @@ import os.path
 import ranger.api
 import ranger.core.fm
 import ranger.ext.signals
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, run
 
 hook_init_prev = ranger.api.hook_init
 
 def hook_init(fm):
     def zoxide_add(signal):
-        path = signal.new.path
-        process = Popen(["zoxide", "add", path])
-        process.wait()
+        Popen(["zoxide", "add", signal.new.path])
 
     fm.signal_bind("cd", zoxide_add)
+    fm.commands.alias("zi", "z -i")
     return hook_init_prev(fm)
 
 
@@ -26,25 +25,28 @@ class z(ranger.api.commands.Command):
     """
     def execute(self):
         results = self.query(self.args[1:])
+        if not results:
+            return
+
         if os.path.isdir(results[0]):
             self.fm.cd(results[0])
 
     def query(self, args):
         try:
-            p = Popen(
-                ["zoxide", "query"] + self.args[1:],
-                stdout=PIPE,
-                stderr=PIPE
-            )
-            stdout, stderr = p.communicate()
-            if p.returncode == 0:
+            zoxide = self.fm.execute_command(f"zoxide query {' '.join(self.args[1:])}",
+                                             stdout=PIPE
+                                             )
+            stdout, stderr = zoxide.communicate()
+
+            if zoxide.returncode == 0:
                 output = stdout.decode("utf-8").strip()
-                if output:
-                    return output.splitlines()
-                else:
-                    self.fm.notify("zoxide exited with status {}".format(p.returncode), bad=True)
+                return output.splitlines()
+            elif zoxide.returncode == 1: # nothing found
+                return None
+            elif zoxide.returncode == 130: # user cancelled
+                return None
             else:
-                output = stderr.decode("utf-8").strip() or "zoxide: unexpected error"
+                output = stderr.decode("utf-8").strip() or f"zoxide: unexpected error (exit code {zoxide.returncode})"
                 self.fm.notify(output, bad=True)
         except Exception as e:
             self.fm.notify(e, bad=True)
@@ -52,3 +54,4 @@ class z(ranger.api.commands.Command):
     def tab(self, tabnum):
         results = self.query(self.args[1:])
         return ["z {}".format(x) for x in results]
+
