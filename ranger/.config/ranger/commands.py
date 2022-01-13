950305409123bf9d23055376b3ddb78a29aca543
c02y <cody.chan.cz@gmail.com>
2022-01-13 20:51:33 +0800

    ranger: delete fzf_locate function and binding

--- ranger/.config/ranger/commands.py
+++ ranger/.config/ranger/commands.py
@@ -350,32 +350,4 @@ class fzf_select(Command):
             else:
                 self.fm.select_file(fzf_file)
 
-
-# fzf_locate
-class fzf_locate(Command):
-    """
-    :fzf_locate
-
-    Find a file using fzf.
-
-    With a prefix argument select only directories.
-
-    See: https://github.com/junegunn/fzf
-    """
-    def execute(self):
-        import subprocess
-        if self.quantifier:
-            command = "locate -P -e -i -d ~/.cache/mlocate.db \"*\" | fzf -e -i"
-        else:
-            command = "locate -P -e -i -d ~/.cache/mlocate.db \"*\" | fzf -e -i"
-        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
-        stdout, stderr = fzf.communicate()
-        if fzf.returncode == 0:
-            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
-            if os.path.isdir(fzf_file):
-                self.fm.cd(fzf_file)
-            else:
-                self.fm.select_file(fzf_file)
-
-
 # ----------End of your own config---------------------------------------------
