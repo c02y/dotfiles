d9be8240ca53f7de5afd0cfb636bb8fa003a180d
c02y <cody.chan.cz@gmail.com>
2022-04-25 18:11:15 +0800

    restore file/config from "CaskaydiaCove Nerd Font Mono" to "Cascadia Mono PL", do not us any icons for exa & icons for ranger, besides there is bug from nerd-fonts-cascadia-code package about whitespace in Emacs

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -502,7 +502,7 @@ It should only modify the values of Spacemacs settings."
    ;; Default font or prioritized list of fonts. The `:size' can be specified as
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
-   dotspacemacs-default-font '("CaskaydiaCove Nerd Font Mono"
+   dotspacemacs-default-font '("Cascadia Mono PL"
                                :size 13.5
                                :weight normal
                                :width normal)
