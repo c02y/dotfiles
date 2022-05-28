bc5d3c74a32f1e5dc81410d35a1f90072ca4e663
c02y <cody.chan.cz@gmail.com>
2022-05-29 04:22:14 +0800

    bin/fish: add toggle_bt_connection.sh script, use it in i3wm from start and another binding

---
+++ bin/.local/bin/toggle_bt_connection.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# bluetoothctl trust 60:AB:D2:0F:72:EB
+# bluetoothctl disconnect 60:AB:D2:0F:72:EB
+
+# Simple version to toggle one specific mac of device
+function toggle_one() {
+	device="60:AB:D2:0F:72:EB"
+	if bluetoothctl info "$device" | grep 'Connected: yes' -q; then
+		bluetoothctl disconnect "$device"
+	else
+		timeout 0.1s bluetoothctl connect "$device"
+	fi
+
+}
+
+devices=$(bluetoothctl devices | awk '{print $2}')
+for device in $devices; do
+	if bluetoothctl info "$device" | grep 'Connected: yes' -q; then
+		bluetoothctl disconnect "$device"
+	else
+		timeout 0.1s bluetoothctl connect "$device"
+		# when device is unavailable, return 1
+		# otherwise, it will connect with return code 124
+		if [[ $? -eq 124 ]]; then
+			# once connect the first device, skip the rest
+			break
+		fi
+	fi
+done
