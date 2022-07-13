c169f7feb2f8f1b2369fcce7b7080218731348d8
c02y <cody.chan.cz@gmail.com>
2022-07-14 04:39:54 +0800

    ranger: add fd_search/next/prev command and config, add dir_history_navigate config and config from ranger github wiki page

--- ranger/.config/ranger/commands.py
+++ ranger/.config/ranger/commands.py
@@ -350,4 +350,141 @@ class fzf_select(Command):
             else:
                 self.fm.select_file(fzf_file)
 
+
+# https://github.com/ranger/ranger/wiki/Custom-Commands#search-with-fd
+from collections import deque
+
+class fd_search(Command):
+    """
+    :fd_search [-d<depth>] <query>
+    Executes "fd -d<depth> <query>" in the current directory and focuses the
+    first match. <depth> defaults to 1, i.e. only the contents of the current
+    directory.
+
+    See https://github.com/sharkdp/fd
+    """
+
+    SEARCH_RESULTS = deque()
+
+    def execute(self):
+        import re
+        import subprocess
+        from ranger.ext.get_executables import get_executables
+
+        self.SEARCH_RESULTS.clear()
+
+        if 'fdfind' in get_executables():
+            fd = 'fdfind'
+        elif 'fd' in get_executables():
+            fd = 'fd'
+        else:
+            self.fm.notify("Couldn't find fd in the PATH.", bad=True)
+            return
+
+        if self.arg(1):
+            if self.arg(1)[:2] == '-d':
+                depth = self.arg(1)
+                target = self.rest(2)
+            else:
+                depth = '-d1'
+                target = self.rest(1)
+        else:
+            self.fm.notify(":fd_search needs a query.", bad=True)
+            return
+
+        hidden = ('--hidden' if self.fm.settings.show_hidden else '')
+        exclude = "--no-ignore-vcs --exclude '.git' --exclude '*.py[co]' --exclude '__pycache__'"
+        command = '{} --follow {} {} {} --print0 {}'.format(
+            fd, depth, hidden, exclude, target
+        )
+        fd = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, _ = fd.communicate()
+
+        if fd.returncode == 0:
+            results = filter(None, stdout.split('\0'))
+            if not self.fm.settings.show_hidden and self.fm.settings.hidden_filter:
+                hidden_filter = re.compile(self.fm.settings.hidden_filter)
+                results = filter(lambda res: not hidden_filter.search(os.path.basename(res)), results)
+            results = map(lambda res: os.path.abspath(os.path.join(self.fm.thisdir.path, res)), results)
+            self.SEARCH_RESULTS.extend(sorted(results, key=str.lower))
+            if len(self.SEARCH_RESULTS) > 0:
+                self.fm.notify('Found {} result{}.'.format(len(self.SEARCH_RESULTS),
+                                                           ('s' if len(self.SEARCH_RESULTS) > 1 else '')))
+                self.fm.select_file(self.SEARCH_RESULTS[0])
+            else:
+                self.fm.notify('No results found.')
+
+class fd_next(Command):
+    """
+    :fd_next
+    Selects the next match from the last :fd_search.
+    """
+
+    def execute(self):
+        if len(fd_search.SEARCH_RESULTS) > 1:
+            fd_search.SEARCH_RESULTS.rotate(-1)  # rotate left
+            self.fm.select_file(fd_search.SEARCH_RESULTS[0])
+        elif len(fd_search.SEARCH_RESULTS) == 1:
+            self.fm.select_file(fd_search.SEARCH_RESULTS[0])
+
+class fd_prev(Command):
+    """
+    :fd_prev
+    Selects the next match from the last :fd_search.
+    """
+
+    def execute(self):
+        if len(fd_search.SEARCH_RESULTS) > 1:
+            fd_search.SEARCH_RESULTS.rotate(1)  # rotate right
+            self.fm.select_file(fd_search.SEARCH_RESULTS[0])
+        elif len(fd_search.SEARCH_RESULTS) == 1:
+            self.fm.select_file(fd_search.SEARCH_RESULTS[0])
+
+# https://github.com/ranger/ranger/wiki/Custom-Commands#navigating-directories-from-ranger-history
+import subprocess
+import os
+
+from ranger.api.commands import Command
+from ranger.ext.get_executables import get_executables
+
+def show_error_in_console(msg, fm):
+    fm.notify(msg, bad=True)
+
+def navigate_path(fm, selected):
+    if not selected:
+        return
+
+    selected = os.path.abspath(selected)
+    if os.path.isdir(selected):
+        fm.cd(selected)
+    elif os.path.isfile(selected):
+        fm.select_file(selected)
+    else:
+        show_error_in_console(f"Neither directory nor file: {selected}", fm)
+        return
+
+def select_with_fzf(fzf_cmd, input, fm):
+    fm.ui.suspend()
+    try:
+        # stderr is used to open to attach to /dev/tty
+        proc = subprocess.Popen(fzf_cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, text=True)
+        stdout, _ = proc.communicate(input=input)
+
+        # ESC gives 130
+        if proc.returncode not in [0, 130]:
+            raise Exception(f"Bad process exit code: {proc.returncode}, stdout={stdout}")
+    finally:
+        fm.ui.initialize()
+    return stdout.strip()
+
+class dir_history_navigate(Command):
+    def execute(self):
+        lst = []
+        for d in reversed(self.fm.tabs[self.fm.current_tab].history.history):
+            lst.append(d.path)
+
+        fm = self.fm
+        selected = select_with_fzf(["fzf"], "\n".join(lst), fm)
+
+        navigate_path(fm, selected)
 # ----------End of your own config---------------------------------------------
