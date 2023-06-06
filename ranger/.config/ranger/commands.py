78d751cf2e1bea7254562758aaf0f9772fc93ffb
c02y <cody.chan.cz@gmail.com>
2023-06-06 15:55:07 +0800

    ranger: add fzf_content_open function to search string in dir with fzf, then bat/vim, add its binding

--- ranger/.config/ranger/commands.py
+++ ranger/.config/ranger/commands.py
@@ -487,4 +487,74 @@ class dir_history_navigate(Command):
         selected = select_with_fzf(["fzf"], "\n".join(lst), fm)
 
         navigate_path(fm, selected)
+
+# https://github.com/ranger/ranger/wiki/Custom-Commands#search-file-content-in-current-directory-recursively-with-fzf-and-open-target-with-neovim
+# NOTE: modified
+class fzf_content_open(Command):
+    """
+    :fzf_content_open
+    Pre-requisites: fzf, rg, bat, awk, vim or neovim
+    Using `rg` to search file content recursively in current directory.
+    Filtering with `fzf` and preview with `bat`.
+    Pressing `Enter` on target will open at line in (neo)vim.
+    """
+
+    def execute(self):
+        import subprocess
+        import os
+        from ranger.ext.get_executables import get_executables
+
+        if 'rg' in get_executables():
+            rg = 'rg'
+        else:
+            self.fm.notify("Couldn't find rg in the PATH.", bad=True)
+            return
+
+        if 'fzf' in get_executables():
+            fzf = 'fzf'
+        else:
+            self.fm.notify("Couldn't find fzf in the PATH.", bad=True)
+            return
+
+        if 'bat' in get_executables():
+            bat = 'bat'
+        else:
+            self.fm.notify("Couldn't find bat in the PATH.", bad=True)
+            return
+
+        editor = None
+        if 'nvim' in get_executables():
+            # NOTE: modified
+            editor = 'vim'
+        elif 'vim' in get_executables():
+            editor = 'vim'
+
+        if rg is not None and fzf is not None and bat is not None and editor is not None:
+            # we should not recursively search through all file content from home directory
+            if (self.fm.thisdir.path == self.fm.home_path):
+                self.fm.notify("Searching from home directory is not allowed", bad=True)
+                return
+            fzf = self.fm.execute_command(
+                'rg --line-number "${1:-.}" | fzf --delimiter \':\' \
+                    --preview \'bat --color=always --highlight-line {2} {1}\' \
+                    | awk -F \':\' \'{print "+"$2" "$1}\'',
+                universal_newlines=True,stdout=subprocess.PIPE)
+
+            stdout, _ = fzf.communicate()
+            if fzf.returncode == 0:
+                if len(stdout) < 2:
+                    return
+
+                selected_line = stdout.split()[0]
+                full_path = stdout.split()[1].strip()
+
+                file_fullpath = os.path.abspath(full_path)
+                file_basename = os.path.basename(full_path)
+
+                if os.path.isdir(file_fullpath):
+                    self.fm.cd(file_fullpath)
+                else:
+                    self.fm.select_file(file_fullpath)
+
+                self.fm.execute_command(editor + " " + selected_line + " " + file_basename)
 # ----------End of your own config---------------------------------------------
