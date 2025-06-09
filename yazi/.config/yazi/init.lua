d39ab4cd8b4cb518290e091169be8d21a431ad63
c02y <cody.chan.cz@gmail.com>
2025-06-09 16:19:55 +0800

    yazi: update the plugin name of smart-filter

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,5 +1,5 @@
 -- TODO: https://github.com/AnirudhG07/awesome-yazi
--- ya pkg -a hankertrix/augment-command Ape/open-with-cmd yazi-rs/plugins#smart-filter ndtoan96/ouch pirafrank/what-size Lil-Dank/lazygit yazi-rs/plugins:toggle-pane yazi-rs/plugins:mount
+-- ya pkg -a hankertrix/augment-command Ape/open-with-cmd yazi-rs/plugins:smart-filter ndtoan96/ouch pirafrank/what-size Lil-Dank/lazygit yazi-rs/plugins:toggle-pane yazi-rs/plugins:mount
 -- ya pkg -i, ya pkg -u
 
 -- https://github.com/sxyazi/yazi/issues/865
